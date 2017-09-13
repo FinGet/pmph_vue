af20b9355b62209353142d7a4b68b93eba87db66
huangwei9527 <1240413812@qq.com>
2017-09-13 13:35:16 +0800

    新增高级搜索区域

--- src/pages/home/teachingMaterial/navContent/pressCheck.vue
+++ src/pages/home/teachingMaterial/navContent/pressCheck.vue
@@ -1,85 +1,125 @@
 <template>
   <div class="teachingMaterial publisherAudit">
-    <div class="teachingMaterial-search clearfix">
-      <!--书名选择框-->
-      <div class="searchBox-wrapper lg">
-        <div class="searchName">书名：<span></span></div>
-        <div class="searchInput">
-          <el-input placeholder="全部" class="searchInputEle"></el-input>
+    <div class="teachingMaterial-search clearfix" :class="{powerSearch:powerSearch}">
+      <div v-if="!powerSearch">
+        <!--书名选择框-->
+        <div class="searchBox-wrapper lg">
+          <div class="searchName">书名：<span></span></div>
+          <div class="searchInput">
+            <el-select v-model="booksChooseValue5" multiple placeholder="请选择">
+              <el-option
+                v-for="item in booksChooseOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+        <!--姓名搜索-->
+        <div class="searchBox-wrapper">
+          <div class="searchName">职务：<span></span></div>
+          <div class="searchInput">
+            <el-input placeholder="全部" class="searchInputEle"></el-input>
+          </div>
+        </div>
+        <!--职务搜索-->
+        <div class="searchBox-wrapper">
+          <div class="searchName">职务：<span></span></div>
+          <div class="searchInput">
+            <el-input placeholder="全部" class="searchInputEle"></el-input>
+          </div>
+        </div>
+        <!--职称搜索-->
+        <div class="searchBox-wrapper">
+          <div class="searchName">职称：<span></span></div>
+          <div class="searchInput">
+            <el-input placeholder="全部" class="searchInputEle"></el-input>
+          </div>
+        </div>
+        <!--工作单位搜索-->
+        <div class="searchBox-wrapper">
+          <div class="searchName">工作单位：<span></span></div>
+          <div class="searchInput">
+            <el-input placeholder="全部" class="searchInputEle"></el-input>
+          </div>
+        </div>
+        <!--申报职务搜索-->
+        <div class="searchBox-wrapper">
+          <div class="searchName">申报职务：<span></span></div>
+          <div class="searchInput">
+            <el-select v-model="positionValueChoose" placeholder="请选择">
+              <el-option
+                v-for="item in positionValue"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+        <!--学校审核搜索-->
+        <!--<div class="searchBox-wrapper">-->
+          <!--<div class="searchName">学校审核：<span></span></div>-->
+          <!--<div class="searchInput">-->
+            <!--<el-input placeholder="全部" class="searchInputEle"></el-input>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--纸质表搜索-->
+        <!--<div class="searchBox-wrapper">-->
+          <!--<div class="searchName">纸质表：<span></span></div>-->
+          <!--<div class="searchInput">-->
+            <!--<el-select v-model="positionValueChoose" placeholder="请选择">-->
+              <!--<el-option-->
+                <!--v-for="item in positionValue"-->
+                <!--:key="item.value"-->
+                <!--:label="item.label"-->
+                <!--:value="item.value">-->
+              <!--</el-option>-->
+            <!--</el-select>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--姓名搜索-->
+        <div class="searchBox-wrapper searchBtn">
+          <el-button  type="primary" icon="search">搜索</el-button>
+        </div>
+        <!--姓名搜索-->
+        <div class="searchBox-wrapper searchBtn">
+          <el-button type="text" @click="toggleSearchType">收起搜索区</el-button>
+        </div>
+        <!--操作按钮-->
+        <div class="operation-wrapper">
+          <el-button type="primary">导出World</el-button>
+          <el-button type="primary">导出Excel</el-button>
         </div>
       </div>
-      <!--姓名搜索-->
-      <div class="searchBox-wrapper">
-        <div class="searchName">职务：<span></span></div>
-        <div class="searchInput">
-          <el-input placeholder="全部" class="searchInputEle"></el-input>
-        </div>
-      </div>
-      <!--职务搜索-->
-      <div class="searchBox-wrapper">
-        <div class="searchName">职务：<span></span></div>
-        <div class="searchInput">
-          <el-input placeholder="全部" class="searchInputEle"></el-input>
-        </div>
-      </div>
-      <!--职称搜索-->
-      <div class="searchBox-wrapper">
-        <div class="searchName">职称：<span></span></div>
-        <div class="searchInput">
-          <el-input placeholder="全部" class="searchInputEle"></el-input>
-        </div>
-      </div>
-      <!--工作单位搜索-->
-      <div class="searchBox-wrapper">
-        <div class="searchName">工作单位：<span></span></div>
-        <div class="searchInput">
-          <el-input placeholder="全部" class="searchInputEle"></el-input>
-        </div>
-      </div>
-      <!--申报职务搜索-->
-      <div class="searchBox-wrapper">
-        <div class="searchName">申报职务：<span></span></div>
-        <div class="searchInput">
-          <el-select v-model="positionValueChoose" placeholder="请选择">
+      <!--高级搜索-->
+      <div v-else>
+        <div class="searchBox-wrapper" :class="{lg : powerSearchValue===1}">
+          <el-select v-model="powerSearchValue" class="searchName" placeholder="请选择">
             <el-option
-              v-for="item in positionValue"
+              v-for="item in powerSearchList"
               :key="item.value"
               :label="item.label"
               :value="item.value">
             </el-option>
           </el-select>
+          <div class="searchInput">
+            <el-input placeholder="全部" class="searchInputEle"></el-input>
+          </div>
+        </div>
+        <div class="searchBox-wrapper searchBtn">
+          <el-button  type="primary" icon="search">搜索</el-button>
+        </div>
+        <div class="searchBox-wrapper searchBtn">
+          <el-button type="text" @click="toggleSearchType">高级搜索</el-button>
+        </div>
+        <!--操作按钮-->
+        <div class="operation-wrapper">
+          <el-button type="primary">导出World</el-button>
+          <el-button type="primary">导出Excel</el-button>
         </div>
       </div>
-      <!--学校审核搜索-->
-      <!--<div class="searchBox-wrapper">-->
-        <!--<div class="searchName">学校审核：<span></span></div>-->
-        <!--<div class="searchInput">-->
-          <!--<el-input placeholder="全部" class="searchInputEle"></el-input>-->
-        <!--</div>-->
-      <!--</div>-->
-      <!--纸质表搜索-->
-      <!--<div class="searchBox-wrapper">-->
-        <!--<div class="searchName">纸质表：<span></span></div>-->
-        <!--<div class="searchInput">-->
-          <!--<el-select v-model="positionValueChoose" placeholder="请选择">-->
-            <!--<el-option-->
-              <!--v-for="item in positionValue"-->
-              <!--:key="item.value"-->
-              <!--:label="item.label"-->
-              <!--:value="item.value">-->
-            <!--</el-option>-->
-          <!--</el-select>-->
-        <!--</div>-->
-      <!--</div>-->
-      <!--姓名搜索-->
-      <div class="searchBox-wrapper searchBtn">
-        <el-button  type="primary" icon="search">搜索</el-button>
-      </div>
-    </div>
-    <!--操作按钮-->
-    <div class="operation-wrapper">
-      <el-button type="primary">导出World</el-button>
-      <el-button type="primary">导出Excel</el-button>
     </div>
     <!--表格-->
     <div class="table-wrapper">
@@ -88,9 +128,11 @@
         border
         style="width: 100%">
         <el-table-column
-          prop="name"
-          sortable
           label="姓名">
+          <template scope="scope">
+            {{scope.row.name}}
+            <i class="fa fa-mars"></i>
+          </template>
         </el-table-column>
         <el-table-column
           prop="code"
@@ -149,6 +191,29 @@
   export default {
     data() {
       return {
+        powerSearch:true,
+        powerSearchList:[
+          {
+            value:1,
+            label:'书名'
+          },{
+            value:2,
+            label:'姓名'
+          },{
+            value:3,
+            label:'职务'
+          },{
+            value:4,
+            label:'职称'
+          },{
+            value:5,
+            label:'工作单位'
+          },{
+            value:6,
+            label:'申报职务'
+          }
+        ],
+        powerSearchValue:2,
         positionValue:[{
           value:1,
           label:'全部'
@@ -162,6 +227,26 @@
           value:4,
           label:'编委'
         }],
+        booksChooseOptions: [{
+          value: '选项1',
+          label: '儿科护理学'
+        }, {
+          value: '选项2',
+          label: '病理学与病理生理学'
+        }, {
+          value: '选项3',
+          label: '精神科护理学'
+        }, {
+          value: '选项4',
+          label: '护理伦理与法律法规'
+        }, {
+          value: '选项6',
+          label: '精神科护理伦理与法律法规'
+        }, {
+          value: '选项5',
+          label: '眼耳鼻咽喉口腔科护理学'
+        }],
+        booksChooseValue5: [],
         positionValueChoose:1,
         tableData: [{
           name: '王小虎',
@@ -282,6 +367,9 @@
       },
       filterPressCheck(value, row){
         return row.pressCheck === value;
+      },
+      toggleSearchType(){
+        this.powerSearch=!this.powerSearch;
       },
     },
   }
