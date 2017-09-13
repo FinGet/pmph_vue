b141112e98b18218720e4cb210d2da3c5b9b384f
huangwei9527 <1240413812@qq.com>
2017-09-13 15:58:15 +0800

    遴选主编

--- src/pages/home/teachingMaterial/navContent/booksSelect.vue
+++ src/pages/home/teachingMaterial/navContent/booksSelect.vue
@@ -1,6 +1,115 @@
 <template>
-    <div class="notice_manage">
-     23232
+    <div class="teachingMaterial bookSelect">
+      <div class="teachingMaterial-search clearfix">
+        <!--书名选择框-->
+        <div class="searchBox-wrapper lg">
+          <div class="searchName">书名：<span></span></div>
+          <div class="searchInput">
+            <el-select v-model="booksChooseValue5" multiple placeholder="全部">
+              <el-option
+                v-for="item in booksChooseOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+        <!--申报职务搜索-->
+        <div class="searchBox-wrapper">
+          <div class="searchName">处理状态：<span></span></div>
+          <div class="searchInput">
+            <el-select v-model="currentState" placeholder="全部">
+              <el-option
+                v-for="item in currentStateList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+        <!--姓名搜索-->
+        <div class="searchBox-wrapper searchBtn">
+          <el-button  type="primary" icon="search">搜索</el-button>
+        </div>
+        <!--操作按钮-->
+        <div class="operation-wrapper">
+          <el-button type="primary">结果公布</el-button>
+          <el-button type="primary">主编副主编确认</el-button>
+        </div>
+      </div>
+      <!--表格-->
+      <div class="table-wrapper">
+        <el-table
+          ref="multipleTable"
+          :data="tableData"
+          tooltip-effect="dark"
+          style="width: 100%">
+          <el-table-column
+            type="selection"
+            width="55">
+          </el-table-column>
+          <el-table-column
+            prop="bookorder"
+            sortable
+            label="书序"
+            width="88">
+          </el-table-column>
+          <el-table-column
+            sortable
+            prop="edition"
+            label="版次"
+            width="88">
+          </el-table-column>
+          <el-table-column
+            prop="bookname"
+            label="书名">
+          </el-table-column>
+          <el-table-column
+            sortable
+            prop="applyNumber"
+            label="申报数"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            label="策划编辑">
+            <template scope="scope">
+              <p v-if="scope.row.planningEditor">
+                {{scope.row.planningEditor}}
+                <el-button type="text">
+                  <i class="fa fa-pencil fa-fw"></i>
+                </el-button>
+              </p>
+              <p class="gray" v-else>( 空 )</p>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="主编/副主编">
+            <template scope="scope">
+              <p v-if="scope.row.chiefEditor">
+                {{scope.row.planningEditor}}等{{1+scope.row.subeditor.length}}人
+                <el-button type="text">
+                  <i class="fa fa-pencil fa-fw" :class="{gray:scope.row.editingNum>0}"></i>
+                </el-button>
+                <el-button type="text">确定</el-button>
+                <el-button type="text">发布</el-button>
+              </p>
+              <p class="gray" v-else>( 空 )</p>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="审核操作">
+            <template scope="scope">
+              <el-button type="text">通过</el-button>
+              <span class="vertical-line"></span>
+              <el-button type="text">结果公布</el-button>
+              <span class="vertical-line"></span>
+              <el-button type="text">导出Excel</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </div>
 </template>
 <style>
@@ -10,7 +119,61 @@
     export default{
         data(){
             return{
-
+              booksChooseValue5:'',
+              booksChooseOptions: [{
+                value: '选项1',
+                label: '儿科护理学'
+              }, {
+                value: '选项2',
+                label: '病理学与病理生理学'
+              }, {
+                value: '选项3',
+                label: '精神科护理学'
+              }, {
+                value: '选项4',
+                label: '护理伦理与法律法规'
+              }, {
+                value: '选项6',
+                label: '精神科护理伦理与法律法规'
+              }, {
+                value: '选项5',
+                label: '眼耳鼻咽喉口腔科护理学'
+              }],
+              currentState:1,
+              currentStateList:[{
+                value:1,
+                label:'全部'
+              },{
+                value:2,
+                label:'待选主编/副主编'
+              },{
+                value:3,
+                label:'已分配主编/副主编'
+              },{
+                value:4,
+                label:'...'
+              }],
+              tableData: [{
+                editingNum:1,
+                bookorder:1,
+                bookname: '神经科学',
+                edition: 9,
+                applyNumber:188,
+                planningEditor:'张强',
+                chiefEditor:'张三',
+                subeditor:['张思','王五','赵六','李思是'],
+                editorialBoard:['张一一','王尔尔','赵三三','一一一','二二人','三三'],
+              },{
+                  editingNum:0,
+                  bookorder:1,
+                  bookname: '神经科学',
+                  edition: 9,
+                  applyNumber:188,
+                  planningEditor:'人卫社',
+                  chiefEditor:'张三',
+                  subeditor:['张思','王五','赵六','李思是'],
+                  editorialBoard:['张一一','王尔尔','赵三三','一一一','二二人','三三'],
+                }],
             }
         }
     }
