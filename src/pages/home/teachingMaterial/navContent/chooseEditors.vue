f64252b345595eef301bcaa6f2a75333829e43ab
Mario <zhangbiaoFT@163.com>
2017-09-13 18:05:22 +0800

    历史记录

--- src/pages/home/teachingMaterial/navContent/chooseEditors.vue
+++ src/pages/home/teachingMaterial/navContent/chooseEditors.vue
@@ -1,5 +1,110 @@
 <template>
     <div class="teachMaterial">
+      <div class="history" ref="history">
+        <div class="history-icon" @click="showHistory"></div>
+        <div class="history-list" ref="historyList">
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+        </div>
+      </div>
       <el-row>
         <el-col :span="24">
           <el-col :span="18">
@@ -22,7 +127,7 @@
               <el-button class="btn" type="primary"  icon="search">搜索</el-button>
               <div class="pull-right">
                 <el-button class="btn" type="primary" >拟定编委</el-button>
-                <el-button class="btn" type="danger" @click="Ldelet">移除</el-button>
+                <el-button class="btn" type="danger" @click="Ldelet" :disabled="isSelected">移除</el-button>
                 <el-button class="btn" type="primary" >增加候选人</el-button>
               </div>
             </el-col>
@@ -63,10 +168,10 @@
                 >
                   <template scope="scope">
                     <el-tooltip :content="'申报单位:'+scope.row.applicationOrganization" placement="top-start">
-                      <p><i class="fa fa-university"></i> {{scope.row.applicationOrganization}}</p>
+                      <p><i class="fa fa-university fa-fw"></i> {{scope.row.applicationOrganization}}</p>
                     </el-tooltip>
                     <el-tooltip :content="'工作单位:'+scope.row.workOrganization" placement="top-start">
-                      <p><i class="fa fa-briefcase"></i> {{scope.row.workOrganization}}</p>
+                      <p><i class="fa fa-briefcase fa-fw"></i> {{scope.row.workOrganization}}</p>
                     </el-tooltip>
                   </template>
                 </el-table-column>
@@ -75,19 +180,20 @@
                 >
                   <template scope="scope">
                     <el-tooltip :content="'职务:'+scope.row.duty" placement="top-start">
-                      <p><i class="fa fa-tags"></i> {{scope.row.duty}}</p>
+                      <p><i class="fa fa-tags fa-fw"></i> {{scope.row.duty}}</p>
                     </el-tooltip>
                     <el-tooltip :content="'职称:'+scope.row.jobTitle" placement="top-start">
-                      <p><i class="fa fa-graduation-cap"></i> {{scope.row.jobTitle}}</p>
+                      <p><i class="fa fa-graduation-cap fa-fw"></i> {{scope.row.jobTitle}}</p>
                     </el-tooltip>
                   </template>
                 </el-table-column>
                 <el-table-column
                   label="联系方式"
+                  width="200"
                 >
                   <template scope="scope">
-                    <p><i class="fa fa-phone"></i> {{scope.row.mobile}}</p>
-                    <p><i class="fa fa-at"></i> {{scope.row.email}}</p>
+                    <p><i class="fa fa-phone fa-fw"></i> {{scope.row.mobile}}</p>
+                    <p><i class="fa fa-envelope fa-fw"></i> {{scope.row.email}}</p>
                   </template>
                 </el-table-column>
                 <el-table-column
@@ -154,6 +260,7 @@
       return {
         input:'',
         input1:'',
+        showhistory: false,
         multipleSelection: [],
         options: [{
           value: '选项1',
@@ -307,6 +414,19 @@
         ]
       }
     },
+    computed:{
+      /**
+       * 判断当前是否有选中项来设置删除按钮是否可以点击
+       * @returns {boolean}
+       */
+      isSelected() {
+        if (this.multipleSelection.length > 0) {
+          return false
+        } else {
+          return true
+        }
+      }
+    },
     methods:{
       // 点击添加
       add(index) {
@@ -333,7 +453,16 @@
         this.$refs.multipleTable.clearSelection()
       },
       Search(){
-
+      },
+      // 展示历史记录
+      showHistory() {
+        if(!this.showhistory){
+          this.$refs.history.style.right = 0
+          this.showhistory = !this.showhistory
+        }else{
+          this.$refs.history.style.right = `-500px`
+          this.showhistory = !this.showhistory
+        }
       }
     }
   }
@@ -346,5 +475,33 @@
     float: left;
     height:36px;
     line-height: 36px;
+  }
+  .history{
+    position: fixed;
+    right:-500px;
+    top:50%;
+    z-index: 1000;
+    width: 500px;
+    height: 600px;
+    margin-top: -300px;
+    /*border:1px solid rgba(42,63,84,.7);*/
+    transition: all .28s;
+    /*overflow: hidden;*/
+  }
+  .history-list{
+    width: 500px;
+    height: 600px;
+    background: rgba(42,63,84,.3);
+    overflow-y: scroll;
+  }
+  .history-icon{
+    position: absolute;
+    left: -17px;
+    top:300px;
+    margin-top: -57px;
+    width: 17px;
+    height: 115px;
+    cursor: pointer;
+    background: url("../../../../common/images/history-icon.png");
   }
 </style>
