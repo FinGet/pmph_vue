c04f9fff277451165105f4b5cba7dca65065d692
AzureCa <18428368747@sina.cn>
2017-09-13 09:41:54 +0800

    路由配置

---
+++ src/pages/home/teachingMaterial/ApplicationRouter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="application_router">
+      <router-view></router-view>
+      </div>   
+</template>
+<style>
+
+</style>
+
+<script type="text/javascript">
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
