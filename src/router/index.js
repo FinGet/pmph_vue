a93577efa41a8b39aafc5b9153b1cbedb0c9c817
AzureCa <18428368747@sina.cn>
2017-09-15 14:11:12 +0800

    通知详情添加

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import ApplicationNav from '../pages/home/teachingMaterial/ApplicationNav'
 import PressCheck from '../pages/home/teachingMaterial/navContent/pressCheck'
 import ExpertInfo from '../pages/home/teachingMaterial/navContent/expertInfo'
 import newBookRelease from '../pages/home/teachingMaterial/navContent/newBookRelease'
+import InfoDetail from '../pages/home/teachingMaterial/navContent/infoDetail'
 import ChooseEditors from 'pages/home/teachingMaterial/navContent/chooseEditors'
 import NewChooseBooks from 'pages/home/teachingMaterial/navContent/newChooseBooks'
 import MessageList from 'pages/home/systemMessage/MessageList'
@@ -35,6 +36,7 @@ export default new Router({
          children:[
             {path:'applicationlist',name:'教材申报',component:ApplicationList, meta: { breadNumber: 2 }},
             {path:'newbookrelease',name:'设置书目录',component:newBookRelease, meta: { breadNumber: 2 }},
+            {path:'infodetail',name:'通知详情',component:InfoDetail, meta: { breadNumber: 2 }},
             {
               path: 'applicationnav', name: '教材申报', component: ApplicationNav, children: [
                 { path: 'booksselectroot', name: '教材遴选', component: BooksSelect, meta: { breadNumber: 3,applicationName:'booksselect' } },
