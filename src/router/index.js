a954394b5355abf9530f3a9cf0d239c966ef9750
AzureCa <18428368747@sina.cn>
2017-09-18 16:46:55 +0800

    部分路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,10 @@ import BooksSelect from '../pages/home/teachingMaterial/navContent/booksSelect'
 import ApplicationNav from '../pages/home/teachingMaterial/ApplicationNav'
 import PressCheck from '../pages/home/teachingMaterial/navContent/pressCheck'
 import ExpertInfo from '../pages/home/teachingMaterial/navContent/expertInfo'
-import newBookRelease from '../pages/home/teachingMaterial/navContent/newBookRelease'
-import InfoDetail from '../pages/home/teachingMaterial/navContent/infoDetail'
-import ChooseEditors from 'pages/home/teachingMaterial/navContent/chooseEditors'
-import ChooseMember from 'pages/home/teachingMaterial/navContent/chooseMember'
+import BookList from '../pages/home/teachingMaterial/navContent/BookList'
+import Notice from '../pages/home/teachingMaterial/navContent/Notice'
+import Chief from '../pages/home/teachingMaterial/navContent/Chief'
+import preSelect from '../pages/home/teachingMaterial/navContent/preSelect'
 import NewChooseBooks from 'pages/home/teachingMaterial/navContent/newChooseBooks'
 import MessageList from 'pages/home/systemMessage/MessageList'
 import MessageState from 'pages/home/systemMessage/MessageState'
@@ -26,6 +26,7 @@ import UserRouter from '../pages/home/userManage/userRouter'
 import pmphUser from '../pages/home/userManage/pmphUser'
 import writerUser from '../pages/home/userManage/writerUser'
 import orgUser from '../pages/home/userManage/orgUser'
+
 Vue.use(Router)
 
 export default new Router({
@@ -39,19 +40,18 @@ export default new Router({
         {path:'index',name:'个人中心',component:Index,meta: { breadNumber: 2 }},
         {path:'materialrouter',name:'教材申报',component:ApplicationRouter, meta: { breadNumber: 2 },
          children:[
-            {path:'materials',name:'通知列表',component:ApplicationList, meta: { breadNumber: 2 }},
+            {path:'materials',name:'通知列表',component:ApplicationList, meta: { breadNumber: 3 }},
             
-            {path:'infodetail',name:'通知详情',component:InfoDetail, meta: { breadNumber: 2 }},
+            {path:'notice',name:'通知详情',component:Notice, meta: { breadNumber: 3 }},
             {
               path: 'materialnav', name: '教材申报', component: ApplicationNav, children: [
                 { path: 'booksselect', name: '教材遴选', component: BooksSelect, meta: { breadNumber: 3,applicationName:'booksselect' } },
                 { path: 'presscheck', name: '申报表审核', component: PressCheck, meta: { breadNumber: 3 ,applicationName:'presscheck'} },
                 { path: 'newchoosebooks', name:'新建遴选教材', component:NewChooseBooks,meta:{breadNumber:3,isShowTags:true}},
-                {path:'newbookrelease',name:'设置书目录',component:newBookRelease, meta: { breadNumber: 4 ,isShowTags:true}},
-                {path:'infodetail',name:'通知详情',component:InfoDetail, meta: { breadNumber: 4 ,isShowTags:true}},
+                {path:'book-list',name:'设置书目录',component:BookList, meta: { breadNumber: 4 ,isShowTags:true}},
                 { path: 'expertinfo', name: '专家信息', component: ExpertInfo, meta: { breadNumber: 4 } },
-                { path: 'chooseeditors', name:'遴选主编/副主编', component:ChooseEditors, meta:{breadNumber: 4}},
-                { path: 'choosemember', name:'预选编委', component:ChooseMember, meta:{breadNumber: 4}}
+                { path: 'chief', name:'遴选主编/副主编', component:Chief, meta:{breadNumber: 4}},
+                { path: 'pre-select', name:'预选编委', component:preSelect, meta:{breadNumber: 4}}
               ]
             },
 
