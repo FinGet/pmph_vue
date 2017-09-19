e0562d9cbe6714ea09e43b3d8cefc567561918ac
AzureCa <18428368747@sina.cn>
2017-09-19 16:30:12 +0800

    冗余代码清除

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-  import {mapGetters} from 'vuex'
 	export default {
 		data() {
 			return {
@@ -13,27 +12,19 @@
       }
 		},
     computed:{
-      ...mapGetters([
-        'breadcrumb'
-      ]),
     },
     watch: {
       $route () {
         this.initBreadData();
-        this.$store.commit('BREADCRUM',{
-          path:this.$route.path,
-          name:this.$route.name,
-          breadNumber:typeof(this.$route.meta.breadNumber)!="undefined"?this.$route.meta.breadNumber:1//面包屑位置索引放到meta里预设好，首页索引为0，一级默认为1
-        });
       }
     },
     methods:{
+      //面包屑
       initBreadData(){
        this.breadData=this.$router.currentRoute.matched;
       }
     },
     created(){
-      console.log(this.$router);
       this.initBreadData();
     }
 	}
