6c46c65e1ef294719c2951b2b6ccd8b7dffd1f3e
Mario <zhangbiaoFT@163.com>
2017-09-21 16:32:56 +0800

    小组功能成员管理

---
+++ src/components/group/GroupMembers.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="groupmember">
+    <el-row>
+      <el-col :span="24">
+        <div class="pull-left">共有{{memberCount}}个成员</div>
+        <div class="pull-right clearfix">
+          <div class="disinline">
+            <el-input></el-input>
+          </div>
+          <div class="disinline">
+            <el-button type="primary" @click="setManage">设为管理员</el-button>
+          </div>
+          <div class="disinline marginL10">
+            <el-popover
+              ref="popover"
+              placement="top"
+              width="160"
+              v-model="visible">
+              <p>选中的文件确定删除吗？</p>
+              <div style="text-align: right; margin: 0">
+                <el-button size="mini" type="text" @click="visible = false">取消</el-button>
+                <el-button type="primary" size="mini" @click="visible = false,deleted()">确定</el-button>
+              </div>
+            </el-popover>
+            <el-button type="danger"  v-popover:popover :disabled="idSelected">删除</el-button>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="24" class="marginT10">
+        <el-table
+          ref="multipleTable"
+          :data="tableData"
+          border
+          tooltip-effect="dark"
+          style="width: 100%"
+          @selection-change="handleSelectionChange">
+          <el-table-column
+            type="selection"
+            width="55">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="工作单位"
+            show-overflow-tooltip>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {
+      tableData:[
+        {
+          name: '张三',
+          address: '成都中医药大学'
+        },
+        {
+          name: '张三',
+          address: '成都中医药大学'
+        },
+        {
+          name: '张三',
+          address: '成都中医药大学'
+        },
+        {
+          name: '张三',
+          address: '成都中医药大学'
+        },{
+          name: '张三',
+          address: '成都中医药大学'
+        },
+        {
+          name: '张三',
+          address: '成都中医药大学'
+        }
+      ],
+      selections: {},
+      visible: false,
+    }
+  },
+  computed: {
+    memberCount() {
+      if (this.tableData.length == 0) {
+        return 0
+      } else {
+        return this.tableData.length
+      }
+    },
+    idSelected() {
+      if (this.selections.length > 0) {
+        return false
+      } else {
+        return true
+      }
+    }
+  },
+  methods: {
+    handleSelectionChange (val) {
+      this.selections = val
+    },
+    // 设置管理员
+    setManage () {
+
+    },
+    // deleted
+    deleted () {
+      this.tableData.splice(this.selections,this.selections.length)
+      this.$refs.multipleTable.clearSelection()
+      this.$message({
+        message: `恭喜你，成功删除数据`,
+        type: 'success'
+      });
+    }
+  }
+}
+</script>
+
+<style>
+  .groupmember{
+    padding: 10px 30px;
+  }
+  .disinline{
+    display: inline-block;
+  }
+</style>
