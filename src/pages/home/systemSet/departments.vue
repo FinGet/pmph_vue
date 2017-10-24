3d63f6baa7758598355d9b5ebd3d9e48193bb592
AzureCa <18428368747@sina.cn>
2017-10-24 15:14:44 +0800

    社内用户数据绑定

--- src/pages/home/systemSet/departments.vue
+++ src/pages/home/systemSet/departments.vue
@@ -7,11 +7,11 @@
           <p class="page-title">人民卫生出版社：</p>
           <!--操作按钮-->
           <div class="pull-right">
-            <el-button type="primary" @click="add" :disabled="!hasSelected">添加子节点</el-button>
-            <el-button type="danger" @click="del" :disabled="!hasSelected">删除</el-button>
+            <el-button type="primary" @click="openAddDialog" :disabled="!hasSelected">添加子节点</el-button>
+            <el-button type="danger" @click="deleteNode" :disabled="!hasSelected">删除</el-button>
           </div>
         </div>
-        <el-tree :data="data"
+        <el-tree :data="treeData"
                  :props="defaultProps"
                  node-key="id"
                  :highlight-current="true"
@@ -25,192 +25,214 @@
     </el-col>
     <el-col :span="12">
       <div class="paddingL30">
-        <p class="page-title paddingB20">部门信息：</p>
+        <p class="page-title paddingB20">编辑部门信息：</p>
         <div class="max-width-460">
-          <el-form label-position="left" label-width="80px" :model="form">
-            <el-form-item label="部门名称">
-              <el-input v-model="form.label" :disabled="!hasSelected"></el-input>
+          <el-form label-position="right" :rules="dialogRules" ref="editForm" label-width="80px" :model="selectObj">
+            <el-form-item label="部门名称" prop="dpName">
+              <el-input  :disabled="!hasSelected" v-model="selectObj.dpName"></el-input>
             </el-form-item>
-            <el-form-item label="显示顺序">
-              <el-input v-model="form.rank" :disabled="!hasSelected"></el-input>
+            <el-form-item label="显示顺序" prop="sort">
+              <el-input  :disabled="!hasSelected" v-model.number="selectObj.sort"></el-input>
             </el-form-item>
             <el-form-item label="备注">
-              <el-input v-model="form.remark" :disabled="!hasSelected"></el-input>
+              <el-input  :disabled="!hasSelected" v-model="selectObj.note"></el-input>
             </el-form-item>
           </el-form>
           <!--操作按钮-->
           <div class="pull-right">
-            <el-button type="primary" @click="submit" :disabled="!hasSelected">保存</el-button>
+            <el-button type="primary"  @click="editSubmit" :disabled="!hasSelected">保存</el-button>
           </div>
         </div>
       </div>
     </el-col>
+
+    <!-- 添加弹框 -->
+       <el-dialog title="新增节点" :visible.sync="dialogVisible" size="tiny">
+         <el-form :model="dialogForm" :rules="dialogRules" ref="dialogForm" label-width="110px">
+           <el-form-item label="部门名称：" prop="dpName">
+               <el-input placeholder="请填写部门名称" v-model="dialogForm.dpName"></el-input>
+           </el-form-item>
+           <el-form-item label="显示顺序：" prop="sort">
+               <el-input placeholder="请填写阿拉伯数字" v-model.number="dialogForm.sort"></el-input>
+           </el-form-item>
+           <el-form-item label="备注：">
+               <el-input v-model="dialogForm.note"></el-input>
+           </el-form-item>
+         </el-form>
+
+         <div slot="footer" class="dialog-footer">
+           <el-button @click="dialogVisible = false">取 消</el-button>
+           <el-button type="primary" @click="addTreeNode">确 定</el-button>
+         </div>
+      </el-dialog>  
   </el-row>
 </template>
 <script>
-  export default{
-    data(){
-      return {
-        hasSelected:false,
-        multipleSelection: [],
-        dialogVisible:false,
-        currentPage: 1,
-        data: [{
-          id: 1,
-          label: '人民卫生出版社',
-          children:[{
-          id:11,
-          code:Math.round(Math.random()*100000),
-          rank:2,
-          remark:'',
-          label: '医学学术编辑中心（期刊编辑出版社）',
-          children: [{
-              code:Math.round(Math.random()*100000),
-              rank:3,
-              remark:'',
-              label: '期刊编辑部'
-            },{
-              code:Math.round(Math.random()*100000),
-              rank:4,
-              remark:'这是我们部门的人',
-              label: '期刊编辑一部'
-            },{
-              code:Math.round(Math.random()*100000),
-              rank:2,
-              remark:'',
-              label: '期刊编辑二部'
-            },{
-              code:Math.round(Math.random()*100000),
-              rank:2,
-              remark:'',
-              label: '期刊编辑三部'
-            }]
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:4,
-            remark:'这是我们部门的人',
-            label: '人卫电子音像公司'
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:3,
-            remark:'',
-            label: '药学中心'
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '智慧数字中心'
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '研发中心'
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '医学教育中心(医学教育研究中心)',
-            children: [{
-              code:Math.round(Math.random()*100000),
-              rank:2,
-              remark:'',
-              label: '职业教育部（护理教育部）'
-            },{
-              label: '高等医学教育部'
-            },{
-              code:Math.round(Math.random()*100000),
-              rank:2,
-              remark:'',
-              label: '继续教育部'
-            }]
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '国际中心'
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '总编辑总经理办公室'
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '人卫健康传播中心',
-            children: [{
-              code:Math.round(Math.random()*100000),
-              rank:2,
-              remark:'',
-              label: '健康传播综合部'
-            },{
-              code:Math.round(Math.random()*100000),
-              rank:2,
-              remark:'',
-              label: '预防医学编辑部'
-            },{
-              code:'1111',
-              rank:2,
-              remark:'',
-              label: '《生活与健康》编辑部'
-            }]
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '中医院中心'
-          },{
-            code:Math.round(Math.random()*100000),
-            rank:2,
-            remark:'',
-            label: '出版社科室1'
-          }]
-        }],
-        form: {
-          code: '',
-          label:'',
-          rank:'',
-          tag:''
-        },
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        },
+export default {
+  data() {
+    return {
+      treeDataUrl: "/departments/pmphdepartmenttree", //获取社内部门树url
+      addNodeUrl: "/departments/add/pmphdepartment", //添加节点url
+      deleteNodelUrl: "/departments/delete/pmphdepartmentbatch", //删除节点url
+      editNodeUrl: "/departments/update/pmphdepartment", //修改节点url
+      treeData: [],
+      hasSelected: false,
+      multipleSelection: [],
+      dialogVisible: false,
+      currentPage: 1,
+      selectObj: {
+        dpName: "",
+        sort: "",
+        note: ""
+      },
+      dialogForm: {
+        dpName: "",
+        sort: "",
+        note: "",
+        parentId: "",
+        path: ""
+      },
+      dialogRules: {
+        dpName: [{ required: true, message: "请填写部门名称", trigger: "blur" }],
+        sort: [
+          { required: true, message: "顺序不能为空" },
+          { type: "number", message: "顺序必须为数字" }
+        ]
+      },
+      defaultProps: {
+        children: "sonDepartment",
+        label: "dpName"
       }
+    };
+  },
+  methods: {
+    /* 获取部门树数据 */
+    getTreeData() {
+      this.$axios
+        .get(this.treeDataUrl, {
+          params: {}
+        })
+        .then(res => {
+          console.log(res);
+          if (res.data.code == 1) {
+            this.treeData = res.data.data.sonDepartment;
+          }
+        });
     },
-    mounted(){
-
+    /* 打开添加子节点对话框 */
+    openAddDialog(){
+     this.dialogVisible=true;
+     this.$nextTick(()=>{
+        this.$refs.dialogForm.resetFields();
+     })
+     
     },
-    methods:{
-      checkNode(data){
-        this.hasSelected=true;
-        for(let key in this.form){
-          this.form[key] = data[key];
+    /* 添加子节点 */
+    addTreeNode() {
+      this.$refs.dialogForm.validate(valid => {
+        if (valid) {
+          this.$axios({
+            method: "POST",
+            url: this.addNodeUrl,
+            data: this.$initPostData(this.dialogForm)
+          }).then(res => {
+            console.log(res);
+            if (res.data.code == 1) {
+              this.getTreeData();
+              this.dialogVisible = false;
+              this.$message.success("添加成功");
+            } else {
+              this.$message.error("添加失败");
+            }
+          });
+        } else {
+          return false;
         }
-        console.log(this.form)
-      },
-      submit(){
-
-      },
-      add(){
-
-      },
-      del(){
-        console.log(this.$refs.tree.getCheckedKeys());
-      },
+      });
+    },
+    /* 删除对应的树节点 */
+    deleteNode() {
+      this.$confirm("确定删除选中节点吗?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$axios
+            .delete(this.deleteNodelUrl, {
+              params: {
+                id: this.selectObj.id
+              }
+            })
+            .then(res => {
+              console.log(res);
+              if (res.data.code == 1) {
+                this.$message.success("删除成功");
+                this.getTreeData();
+              }
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    /* 修改选中节点 */
+    editSubmit() {
+      this.$refs.editForm.validate(valid => {
+        if (valid) {
+          this.$confirm("确定修改当前节点吗？", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+          }).then(() => {
+              this.$axios({
+            method: "PUT",
+            url: this.editNodeUrl,
+            data: this.$initPostData(this.selectObj)
+          }).then((res) => {
+            console.log(res);
+            if(res.data.code==1){
+              this.$message.success('修改成功');
+            }
+          });
+            })
+            .catch(() => {
+              this.$message({
+                type: "info",
+                message: "已取消操作"
+              });
+            });
+        } else {
+          return false;
+        }
+      });
+    },
+    checkNode(data) {
+      this.hasSelected = true;
+      this.dialogForm.path = data.path;
+      this.dialogForm.parentId = data.id;
+      this.selectObj = data;
+      console.log(this.selectObj);
     }
+  },
+  created() {
+    this.getTreeData();
   }
+};
 </script>
 <style>
-  .department{
-    width: 100%;
-    box-sizing: border-box;
-  }
-  .tree-title{
-    line-height: 36px;
-    margin-bottom: 20px;
-  }
-  .max-width-460{
-    max-width: 460px;
-  }
+.department {
+  width: 100%;
+  box-sizing: border-box;
+}
+.tree-title {
+  line-height: 36px;
+  margin-bottom: 20px;
+}
+.max-width-460 {
+  max-width: 460px;
+}
 </style>
