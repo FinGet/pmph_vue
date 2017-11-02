25a4790661eb37e1c282815073289638dd236ba0
AzureCa <18428368747@sina.cn>
2017-11-02 09:06:09 +0800

    日常更新

--- src/pages/home/contentManage/outContentManage.vue
+++ src/pages/home/contentManage/outContentManage.vue
@@ -4,7 +4,6 @@
   <el-tab-pane label="内容">
            <p class="header_p">
           <el-input placeholder="输入文章标题" class="input"></el-input>
-          <el-input placeholder="输入内容" class="input"></el-input>
           <el-select v-model="selectValue" style="width:186px" class="input" placeholder="选择筛选状态">
            <el-option
              v-for="item in selectOp"
