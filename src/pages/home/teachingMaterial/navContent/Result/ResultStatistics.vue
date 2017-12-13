be3816515f7dd4c6fd72d385aeaa981d0741e3da
Mario <zhangbiaoFT@163.com>
2017-12-13 18:09:19 +0800

    小修改

--- src/pages/home/teachingMaterial/navContent/Result/ResultStatistics.vue
+++ src/pages/home/teachingMaterial/navContent/Result/ResultStatistics.vue
@@ -142,7 +142,7 @@ import echarts from "../../../../../../static/echarts/echarts.common.min";
 export default {
   data() {
     return {
-      activeName: "school",
+      activeName: "bookName",
       schoolResultUrl:'/pmpheep/decPosition/list',  //学校结果统计URL
       bookResultUrl: '/pmpheep/decPosition/list/bookList', // 书籍结果统计url
       sortType:true,   //排序方式 true 按当选数排序  false 按申报数排序
