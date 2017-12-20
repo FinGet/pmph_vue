75f0aced8a8c94dafe66a02d6714f3c239644d71
Mario <zhangbiaoFT@163.com>
2017-12-20 15:23:11 +0800

    图书纠错审核

---
+++ src/pages/home/bookError/check.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="check" style="height:100%;">
+		纠错审核
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+
+</style>
