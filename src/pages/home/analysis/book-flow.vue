ebafbbe6e42bf9c61d3f8d4466595190591cf4e6
huangwei9527 <1240413812@qq.com>
2017-12-22 10:37:20 +0800

    数据分析页面设计

--- src/pages/home/analysis/book-flow.vue
+++ src/pages/home/analysis/book-flow.vue
@@ -1,17 +1,173 @@
 <template>
-	<div>
-    图书流量概况
-	</div>
+  <div class="book-flow-page">
+    <div class="page-title text-center">
+      图书流量概况（ 2017-12-20 ）
+    </div>
+
+    <div class="flow-section">
+      <div class="flow-title">
+        <i class="fa  fa-area-chart fa-lg"></i>
+        统计开通日期：2016-12-22
+      </div>
+
+      <!--搜索-->
+      <div class="clearfix paddingT30">
+        <div class="searchBox-wrapper">
+          <div class="searchName">书籍名称：<span></span></div>
+          <div class="searchInput">
+            <el-input placeholder="请输入"></el-input>
+          </div>
+        </div>
+        <div class="searchBox-wrapper searchBtn">
+          <el-button  type="primary" icon="search">搜索</el-button>
+        </div>
+      </div>
+
+      <div class="table-wrapper">
+        <el-table
+          ref="multipleTable"
+          :data="tableData"
+          border
+          stripe
+          tooltip-effect="dark"
+          style="width: 100%">
+          <el-table-column label="统计日期"  prop="date" width="100"></el-table-column>
+          <el-table-column label="浏览次数(PV)"  prop="pv"></el-table-column>
+          <el-table-column label="独立访问(UV)"  prop="uv"></el-table-column>
+          <el-table-column label="IP"  prop="ip"></el-table-column>
+          <el-table-column label="失跳率"  prop="shitiao"></el-table-column>
+          <el-table-column label="访问次数"  prop="num"></el-table-column>
+        </el-table>
+      </div>
+      <div class="table-wrapper">
+        <el-table
+          ref="multipleTable"
+          :data="tableData"
+          border
+          stripe
+          tooltip-effect="dark"
+          style="width: 100%">
+          <el-table-column label="统计日期"  prop="date" width="100"></el-table-column>
+          <el-table-column label="当日回头访客(占比)"  prop="pv"></el-table-column>
+          <el-table-column label="访问平均访问频度"  prop="uv"></el-table-column>
+          <el-table-column label="平均访问时长"  prop="ip"></el-table-column>
+          <el-table-column label="平均访问深度"  prop="shitiao"></el-table-column>
+          <el-table-column label="人均浏览页数"  prop="num"></el-table-column>
+        </el-table>
+      </div>
+    </div>
+
+    <div class="flow-section">
+      <div class="flow-title">
+        <i class="fa  fa-line-chart fa-lg"></i>
+        流量趋势
+      </div>
+      <div class="paddingT20 paddingB20">
+        <span>选择时间：</span>
+        <div class="inline-block">
+          <el-radio-group v-model="time">
+            <el-radio :label="1">今天</el-radio>
+            <el-radio :label="2">昨日</el-radio>
+            <el-radio :label="3">近48小时</el-radio>
+            <el-radio :label="4">近7天(单位:小时)</el-radio>
+            <el-radio :label="5">近30天(单位:小时)</el-radio>
+          </el-radio-group>
+        </div>
+      </div>
+
+      <div class="echart-wrapper">
+        <div id="echarts" style="width: 100%;height:600px;" ref="echarts"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {}
-		}
-	}
+  import echarts from "../../../../static/echarts/echarts.common.min";
+  export default {
+    data() {
+      return {
+        tableData:[{
+          date:'今天',
+          pv:902,
+          uv:631,
+          ip:519,
+          shitiao:'92.34%',
+          num:2179
+        },{
+          date:'昨天',
+          pv:902,
+          uv:631,
+          ip:519,
+          shitiao:'92.34%',
+          num:2179
+        },{
+          date:'今天',
+          pv:902,
+          uv:631,
+          ip:519,
+          shitiao:'92.34%',
+          num:2179
+        }],
+        time:1,
+      }
+    },
+    mounted(){
+      var myChart = echarts.init(this.$refs.echarts);
+      var option = {
+        title: {
+          text: '流量趋势'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data:['近7天']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一','周二','周三','周四','周五','周六','周日']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name:'近7天',
+            type:'line',
+            stack: '总量',
+            data:[120, 132, 101, 134, 90, 230, 210]
+          }
+        ]
+      };
+      myChart.setOption(option);
+    },
+  }
 </script>
 
 <style scoped>
-
+  .flow-title{
+    padding: 10px;
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+    font-size: 16px;
+  }
+  .flow-section{
+    padding-bottom: 30px;
+  }
+  .page-title{
+    font-size: 18px;
+  }
 </style>
--- src/pages/home/analysis/book-preference.vue
+++ src/pages/home/analysis/book-preference.vue
@@ -1,13 +1,47 @@
 <template>
-	<div>
-    用户图书偏好分析
+	<div class="book-preference-page">
+    <!--搜索-->
+    <div class="clearfix">
+      <div class="searchBox-wrapper">
+        <div class="searchName">书籍名称：<span></span></div>
+        <div class="searchInput">
+          <el-input placeholder="请输入"></el-input>
+        </div>
+      </div>
+      <div class="searchBox-wrapper searchBtn">
+        <el-button  type="primary" icon="search">搜索</el-button>
+      </div>
+    </div>
+
+    <!--图表-->
+    <div class="table-wrapper">
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        border
+        stripe
+        tooltip-effect="dark"
+        style="width: 100%">
+        <el-table-column label="图书名称"  prop="bookName"></el-table-column>
+        <el-table-column label="收藏次数"  prop="pv" width="140" align="center"></el-table-column>
+        <el-table-column label="浏览次数"  prop="uv" width="140" align="center"></el-table-column>
+        <el-table-column label="3以上星评价次数"  prop="ip" width="160" align="center"></el-table-column>
+      </el-table>
+    </div>
 	</div>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {}
+			return {
+        tableData:[{
+          bookName:'X线摄影化学及暗室技术',
+          pv:10,
+          uv:7,
+          ip:0
+        }],
+      }
 		}
 	}
 </script>
