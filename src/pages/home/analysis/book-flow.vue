7d4a4b04ef14834e4b3bdbd38b1d693a5c404b66
huangwei9527 <1240413812@qq.com>
2017-12-22 16:17:35 +0800

    广告编辑页面

--- src/pages/home/analysis/book-flow.vue
+++ src/pages/home/analysis/book-flow.vue
@@ -31,78 +31,42 @@
           stripe
           tooltip-effect="dark"
           style="width: 100%">
-          <el-table-column label="统计日期"  prop="date" width="100"></el-table-column>
-          <el-table-column label="浏览次数(PV)"  prop="pv"></el-table-column>
-          <el-table-column label="独立访问(UV)"  prop="uv"></el-table-column>
-          <el-table-column label="IP"  prop="ip"></el-table-column>
-          <el-table-column label="失跳率"  prop="shitiao"></el-table-column>
-          <el-table-column label="访问次数"  prop="num"></el-table-column>
+          <el-table-column label="书籍名称"  prop="bookName" min-width="200"></el-table-column>
+          <el-table-column label="ISBN"  prop="isbn" width="200"></el-table-column>
+          <el-table-column label="浏览次数(PV)"  prop="pv" align="center"></el-table-column>
+          <el-table-column label="独立访问(UV)"  prop="uv" align="center"></el-table-column>
+          <el-table-column label="IP"  prop="ip" align="center"></el-table-column>
+          <el-table-column label="失跳率"  prop="shitiao" align="center"></el-table-column>
+          <el-table-column label="访问次数"  prop="num" align="center"></el-table-column>
         </el-table>
-      </div>
-      <div class="table-wrapper">
-        <el-table
-          ref="multipleTable"
-          :data="tableData"
-          border
-          stripe
-          tooltip-effect="dark"
-          style="width: 100%">
-          <el-table-column label="统计日期"  prop="date" width="100"></el-table-column>
-          <el-table-column label="当日回头访客(占比)"  prop="pv"></el-table-column>
-          <el-table-column label="访问平均访问频度"  prop="uv"></el-table-column>
-          <el-table-column label="平均访问时长"  prop="ip"></el-table-column>
-          <el-table-column label="平均访问深度"  prop="shitiao"></el-table-column>
-          <el-table-column label="人均浏览页数"  prop="num"></el-table-column>
-        </el-table>
-      </div>
-    </div>
-
-    <div class="flow-section">
-      <div class="flow-title">
-        <i class="fa  fa-line-chart fa-lg"></i>
-        流量趋势
-      </div>
-      <div class="paddingT20 paddingB20">
-        <span>选择时间：</span>
-        <div class="inline-block">
-          <el-radio-group v-model="time">
-            <el-radio :label="1">今天</el-radio>
-            <el-radio :label="2">昨日</el-radio>
-            <el-radio :label="3">近48小时</el-radio>
-            <el-radio :label="4">近7天(单位:小时)</el-radio>
-            <el-radio :label="5">近30天(单位:小时)</el-radio>
-          </el-radio-group>
-        </div>
-      </div>
-
-      <div class="echart-wrapper">
-        <div id="echarts" style="width: 100%;height:600px;" ref="echarts"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import echarts from "../../../../static/echarts/echarts.common.min";
   export default {
     data() {
       return {
         tableData:[{
-          date:'今天',
+          bookName:'X线摄影化学及暗室技术',
+          isbn:'7-117-02962-5/R·963',
           pv:902,
           uv:631,
           ip:519,
           shitiao:'92.34%',
           num:2179
         },{
-          date:'昨天',
+          bookName:'X线摄影化学及暗室技术',
+          isbn:'7-117-02962-5/R·963',
           pv:902,
           uv:631,
           ip:519,
           shitiao:'92.34%',
           num:2179
         },{
-          date:'今天',
+          bookName:'X线摄影化学及暗室技术',
+          isbn:'7-117-02962-5/R·963',
           pv:902,
           uv:631,
           ip:519,
@@ -111,48 +75,6 @@
         }],
         time:1,
       }
-    },
-    mounted(){
-      var myChart = echarts.init(this.$refs.echarts);
-      var option = {
-        title: {
-          text: '流量趋势'
-        },
-        tooltip: {
-          trigger: 'axis'
-        },
-        legend: {
-          data:['近7天']
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        toolbox: {
-          feature: {
-            saveAsImage: {}
-          }
-        },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: ['周一','周二','周三','周四','周五','周六','周日']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            name:'近7天',
-            type:'line',
-            stack: '总量',
-            data:[120, 132, 101, 134, 90, 230, 210]
-          }
-        ]
-      };
-      myChart.setOption(option);
     },
   }
 </script>
--- src/pages/home/contentManage/adManage/ad-edit.vue
+++ src/pages/home/contentManage/adManage/ad-edit.vue
@@ -16,13 +16,11 @@
 
     <div class="page-section">
       <div class="section-title">
-        选择样式
+        当前广告
       </div>
       <div class="section-content section-content-arrow">
         <div class="carousel carousel-1">
-          <ul>
-            <li v-for="(iterm,index) in adData.img"><img :src="iterm" alt=""></li>
-          </ul>
+          <img :src="adData.imageList[radio2]" alt="">
         </div>
         <div class="carousel carousel-2"></div>
       </div>
@@ -32,13 +30,31 @@
       <div class="section-title">
         图片管理
         <div class="paddingL20 inline-block">
-          <el-button type="primary" size="mini">增加</el-button>
-          <el-button type="danger" size="mini">批量删除</el-button>
-          <el-button type="warning" size="mini">清空</el-button>
+          <!--<el-button type="primary" size="mini">增加</el-button>-->
         </div>
       </div>
       <div class="section-content section-content-arrow">
+        <el-radio-group v-model="radio2">
+          <el-radio v-for="(iterm,index) in adData.imageList" :key="index" :label="index">
+            <div  class="imageList-iterm">
+              <img :src="iterm" alt="" class="vertical-align-middle" />
+            </div>
+          </el-radio>
+        </el-radio-group>
 
+
+        <my-upload
+          class="fileInput"
+          ref="upload"
+          action="/pmpheep/cms/cmsAdvertisement/addimage"
+          :data="uploadImageData"
+          :on-success="upLoadFileSuccess"
+          :on-error="uploadError"
+          :show-file-list="false">
+          <div class="inline-block add-img-btn-wrapper">
+            <i class="el-icon-plus add-img-btn"></i>
+          </div>
+        </my-upload>
       </div>
     </div>
 
@@ -47,7 +63,23 @@
         全局设置
       </div>
       <div class="section-content section-content-arrow">
+        <el-checkbox v-model="hasTitle">是否显示标题</el-checkbox>
+        <el-checkbox>自动播放</el-checkbox>
+        <div class="inline-block paddingL10">
+          动画间隔（秒）
+          <el-input-number size="small" v-model="num"></el-input-number>
+        </div>
 
+        <div class="ad-edit-input-title" v-if="hasTitle">
+          <el-form label-position="left" label-width="60px" :model="formLabel">
+            <el-form-item label="标题:">
+              <el-input v-model="formLabel.title"></el-input>
+            </el-form-item>
+            <el-form-item label="描述:">
+              <el-input v-model="formLabel.des"></el-input>
+            </el-form-item>
+          </el-form>
+        </div>
       </div>
     </div>
 
@@ -62,10 +94,23 @@
 			  colList:[{id:1,name:'首页公告下方3张图片'}],
         id:1,
         adData:{
-			    img:['http://medu.ipmph.com/pmph_imesp/web/img/banner2d.png']
-        }
+			    img:['http://medu.ipmph.com/pmph_imesp/web/img/banner2d.png'],
+          imageList:['http://medu.ipmph.com/pmph_imesp/web/img/banner2d.png','http://medu.ipmph.com/pmph_imesp/web/img/bannerd.png'],
+        },
+        formLabel:{
+			    title:'',
+          des:'',
+        },
+        radio2:1,
+        num:3,
+        hasTitle:false,
+        uploadImageData:{},
       }
-		}
+		},
+    methods:{
+      upLoadFileSuccess(){},
+      uploadError(){}
+    }
 	}
 </script>
 
@@ -98,4 +143,51 @@
     top: -5px;
     left: 14px;
   }
+  .imageList-iterm{
+    display: inline-block;
+    width: 116px;
+    height: 100px;
+    line-height: 100px;
+    position: relative;
+    text-align: center;
+  }
+  .iterm-img-box{
+    display: inline-block;
+    height: 100%;
+  }
+  .iterm-img-box img{
+    display: inline-block;
+    vertical-align: middle;
+    max-width: 100%;
+    max-height: 100%;
+
+  }
+  .add-img-btn-wrapper{
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    border: 1px dashed #ccc;
+    margin-left: 10px;
+    cursor: pointer;
+    transition: all .28s;
+  }
+  .add-img-btn {
+    font-size: 28px;
+    color: #8c939d;
+    width: 80px;
+    height: 80px;
+    line-height: 80px;
+    text-align: center;
+  }
+  .add-img-btn-wrapper:hover{
+    background: rgba(0,0,0,.3);
+  }
+  .add-img-btn-wrapper:hover i{
+    transition: all .28s;
+    color: #eee;
+  }
+.ad-edit-input-title{
+  max-width: 400px;
+  padding-top: 60px;
+}
 </style>
