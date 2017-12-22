c6e797bc844ffd02a54c0501550dd41aa8ad3e29
AzureCa <18428368747@sina.cn>
2017-12-22 13:40:29 +0800

    日常

---
+++ src/pages/home/questionSurvey/surveyModelSet.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="survey_model_set">
+    调查问卷模板设置
+  </div>
+</template>
+<script type="text/javascript">
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
---
+++ src/pages/home/questionSurvey/surveyRecovery.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="survey_recovery">
+    调查问卷回收
+  </div>
+</template>
+<script type="text/javascript">
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
