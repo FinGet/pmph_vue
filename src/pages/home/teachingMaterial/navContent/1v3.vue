8d132edb16b89e75fcda9d34ba3ff20c53510c60
Mario <zhangbiaoFT@163.com>
2017-12-22 09:07:52 +0800

    更新小组，更新失败提醒

--- src/pages/home/teachingMaterial/navContent/1v3.vue
+++ src/pages/home/teachingMaterial/navContent/1v3.vue
@@ -558,6 +558,8 @@
           let res = response.data;
           if (res.code == 1) {
             this.$message.success('更新成功！');
+          }else {
+            this.$message.error('更新失败！');
           }
         })
       },
