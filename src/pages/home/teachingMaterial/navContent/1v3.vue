785a2d5c4822ea561571a7295f5761da70fb328b
Mario <zhangbiaoFT@163.com>
2017-12-27 13:38:14 +0800

    批量导出主编副主编

--- src/pages/home/teachingMaterial/navContent/1v3.vue
+++ src/pages/home/teachingMaterial/navContent/1v3.vue
@@ -48,7 +48,7 @@
       <!--操作按钮-->
       <div class="operation-wrapper">
         <el-button type="primary" :disabled="!hasAccess(6,myPower)" @click="isForceEnd">{{forceEnd?'恢复':'强制结束'}}</el-button>
-        <el-button type="primary" :disabled="isSelected || !hasAccess(3,myPower) || forceEnd" >主编/副主编批量导出</el-button>
+        <el-button type="primary" :disabled="isSelected || !hasAccess(3,myPower) || forceEnd" @click="exportEditor">主编/副主编批量导出</el-button>
         <el-button type="primary" :disabled="isSelected || !hasAccess(3,myPower) || forceEnd" @click="showDialog(1)">批量名单确认</el-button>
         <el-button type="primary" :disabled="isSelected || !hasAccess(3,myPower) || forceEnd" @click="showDialog(0)">批量结果公布</el-button>
         <el-button type="primary" @click="exportExcel()">批量导出名单</el-button>
@@ -612,9 +612,14 @@
       exportExcel(id){
         // console.log(id,this.selectedIds)
         let url = '/pmpheep/position/exportExcel/?textbookIds='+ (id || this.selectedIds);
-        console.log(url)
+        // console.log(url)
         this.$commonFun.downloadFile(url);
-
+      },
+      /**批量导出主编 */
+      exportEditor(){
+        let url = '/pmpheep/position/exportExcel/?textbookIds=' + this.selectedIds;
+        // console.log(url)
+        this.$commonFun.downloadFile(url);
       },
       /**删除成员 */
       deleteMember(index, rows){
