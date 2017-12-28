5dfa9bca87edc2d00538ab866af93f3eb051eeeb
AzureCa <18428368747@sina.cn>
2017-12-28 14:30:01 +0800

    调查问卷

--- src/pages/home/questionSurvey/surveyRecovery.vue
+++ src/pages/home/questionSurvey/surveyRecovery.vue
@@ -1,17 +1,148 @@
 <template>
   <div class="survey_recovery">
-    调查问卷回收
+    <p class="header_p">
+       <span>调查问卷名称：</span>
+       <el-input class="input" placeholder="请输入调查问卷名称"></el-input>
+       <span>回收日期：</span>
+       <el-date-picker
+            v-model="searchParams.startDate"
+            class="input"
+            type="date"
+            placeholder="请选择开始日期">
+        </el-date-picker>
+        <span>-</span>
+        <el-date-picker
+            v-model="searchParams.endDate"
+            class="input"
+            type="date"
+            placeholder="请选择结束日期">
+        </el-date-picker>
+        <el-button type="primary" icon="search">搜索</el-button>
+    </p>
+    <el-table
+    :data="tableData"
+    style="width:100%"
+    border
+    class="table-wrapper"
+    >
+     <el-table-column
+     label="调查问卷名称"
+     prop="name"
+     >  
+     </el-table-column>
+      <el-table-column
+     label="调查对象"
+     prop="surveyObj"
+     width="100"
+     >  
+     </el-table-column>
+     <el-table-column
+     label="问卷制作人"
+     prop="writer"
+     width="110"
+     >  
+     </el-table-column> 
+     <el-table-column
+     label="问卷概述"
+     prop="summary"
+     >  
+     </el-table-column> 
+     <el-table-column
+     label="回收日期"
+     prop="createDate"
+     width="120"
+     >  
+     </el-table-column>
+     <el-table-column
+      label="操作"
+      width="100"
+     >
+     <template scope="scope">
+       <el-button type="text" @click="$router.push({name:'调查结果统计'})">查看详情</el-button>
+     </template>
+     </el-table-column> 
+    </el-table>
+          <!--分页-->
+    <div class="pagination-wrapper">
+      <el-pagination
+        v-if="pageTotal>searchParams.pageSize"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="searchParams.pageNumber"
+        :page-sizes="[10,20,30,50]"
+        :page-size="searchParams.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pageTotal">
+      </el-pagination>
+    </div>
   </div>
 </template>
 <script type="text/javascript">
     export default{
         data(){
             return{
+                searchParams:{
+                    startDate:'',
+                    endDate:'',
+                    pageSize:10,
+                    pageNumber:1
+                },
+                pageTotal:100,
+                tableData:[
+                    {
+                        name:'求职调查',
+                        surveyObj:'在校学生',
+                        writer:'张三',
+                        summary:'调查学生的职业规划',
+                        createDate:'2017-10-1'
+                    },
+                    {
+                        name:'创业调查',
+                        surveyObj:'在校老师',
+                        writer:'李四',
+                        summary:'调查学生的创业能力',
+                        createDate:'2017-9-1'
+                    },
+                    {
+                        name:'卫生情况调查',
+                        surveyObj:'卫生院系',
+                        writer:'王五',
+                        summary:'调查卫生学校情况',
+                        createDate:'2017-8-1'
+                    },
+                    {
+                        name:'课外活动项目调查',
+                        surveyObj:'在校学生',
+                        writer:'赵六',
+                        summary:'调查学生课外活动参与情况',
+                        createDate:'2017-4-1'
+                    },
+                    {
+                        name:'校园兴趣调查',
+                        surveyObj:'在校学生',
+                        writer:'张三',
+                        summary:'调查学生的校园兴趣',
+                        createDate:'2017-8-5'
+                    },
+                ]
+            }
+        },
+        methods:{
+            handleSizeChange(){
+
+            },
+            handleCurrentChange(){
 
             }
         }
     }
 </script>
-<style>
-
+<style scoped>
+.survey_recovery .header_p {
+  overflow: hidden;
+}
+.survey_recovery .header_p .input {
+  width: 217px;
+  margin-right: 10px;
+}
 </style>
