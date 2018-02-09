06e52ddbcdbbe1361766d3b178be6663f5410d3a
Mario <zhangbiaoFT@163.com>
2018-02-09 17:46:47 +0800

    选题申报权限修改

--- src/pages/home/selectTopic/topicExam.vue
+++ src/pages/home/selectTopic/topicExam.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="topic_exam">
   <el-tabs v-model="activeName" type="border-card" >
-    <el-tab-pane label="转发部门" name="first">
+    <el-tab-pane label="转发部门" name="first" v-if="Identity.isAdmin || Identity.isOpts">
       <forward-depart :activeName.sync='activeName' @changeActive='changeActive'></forward-depart>
     </el-tab-pane>
-    <el-tab-pane label="分配编辑" name="second">
+    <el-tab-pane label="分配编辑" name="second" v-if="Identity.isAdmin || Identity.isDirector">
       <distribute-editor :activeName.sync='activeName' @changeActive='changeActive'></distribute-editor>
     </el-tab-pane>
-    <el-tab-pane label="受理" name="third">
+    <el-tab-pane label="受理" name="third" v-if="Identity.isAdmin || Identity.isEditor">
       <acceptance :activeName.sync='activeName' @changeActive='changeActive'></acceptance>
     </el-tab-pane>
   </el-tabs>
@@ -20,13 +20,27 @@ import acceptance from './acceptance.vue'
  export default{
    data(){
      return{
-      activeName:'first'
+       Identity:{}
      }
    },
    computed:{
+     activeName:{
+      get: function () {
+        if (this.Identity.isAdmin || this.Identity.isOpts) {
+          return 'first';
+        } else if (this.Identity.isAdmin || this.Identity.isDirector) {
+          return 'second';
+        } else if (this.Identity.isAdmin || this.Identity.isEditor) {
+          return 'third';
+        }
+      },
+       set: function () {
+       }
+     }
    },
    created(){
      this.activeName = this.$route.query.active || 'first';
+     this.identity();
    },
    components: {
      forwardDepart,distributeEditor,acceptance
@@ -34,7 +48,15 @@ import acceptance from './acceptance.vue'
    methods:{
     changeActive(val){
       this.activeName=val;
-    }
+    },
+     identity(){
+      this.$axios.get('/pmpheep/topic/identity').then(response=> {
+        let res = response.data;
+        if (res.code == '1') {
+          this.Identity = res.data;
+        }
+      })
+     }
    }
  }
 </script>
