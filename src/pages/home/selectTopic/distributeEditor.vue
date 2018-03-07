301d968bc2f3b368ffc9e00646cef958ad7506fb
AzureCa <18428368747@sina.cn>
2018-03-07 19:19:58 +0800

    BUG修复

--- src/pages/home/selectTopic/distributeEditor.vue
+++ src/pages/home/selectTopic/distributeEditor.vue
@@ -113,7 +113,7 @@
           width="90"
           >
           <template scope="scope">
-           <el-button type="text" @click="distributeSelect(1,scope.row.id)">选择</el-button>
+           <el-button type="text" @click="distributeSelect(1,obj)">选择</el-button>
           </template>
           </el-table-column>
       </el-table>
@@ -254,12 +254,12 @@ export default {
         this.backDialogVislble=true;
     },
 		/**分配部门编辑、退回运维人员 */
-    distributeSelect(i,id){
+    distributeSelect(i,obj){
         this.$confirm('确定分配给该编辑?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
         }).then(() => {
-          this.distributeParams.editorId=id;
+          this.distributeParams.editorId=obj.id;
           this.directorHandling(i);
         }).catch(() => {
           this.$message({
