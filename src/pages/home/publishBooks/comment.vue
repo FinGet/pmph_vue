ae794509eb9c23cf981a322869453d648a10c7f6
FinGet <zhangbiaoFT@163.com>
2018-03-08 10:36:35 +0800

    取消置顶

--- src/pages/home/publishBooks/comment.vue
+++ src/pages/home/publishBooks/comment.vue
@@ -32,6 +32,7 @@
             <!--操作按钮-->
             <div class="pull-right">
               <el-button type="primary" :disabled="!selectData.length" @click="setState('isStick')">置顶</el-button>
+              <el-button type="warning" :disabled="!selectData.length" @click="setState(false)">取消置顶</el-button>
               <el-button type="primary" :disabled="!selectData.length" @click="setState('isPromote')">设为精选</el-button>
               <el-button type="danger" :disabled="!selectData.length" @click="deleteComment">删除</el-button>
               <el-button type="warning" :disabled="!selectData.length" @click="audit(2)">审核不通过</el-button>
@@ -81,6 +82,7 @@
             <!--操作按钮-->
             <div class="pull-right">
               <el-button type="primary" :disabled="!selectData.length" @click="setState('isStick')">置顶</el-button>
+              <el-button type="warning" :disabled="!selectData.length" @click="setState(false)">取消置顶</el-button>
               <el-button type="primary" :disabled="!selectData.length" @click="setState('isPromote')">设为精选</el-button>
               <el-button type="danger" :disabled="!selectData.length" @click="deleteComment">删除</el-button>
               <el-button type="warning" :disabled="!selectData.length" @click="audit(2)">审核不通过</el-button>
