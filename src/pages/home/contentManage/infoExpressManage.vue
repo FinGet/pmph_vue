bf02f10caf7caae7fa3ec628c114c5461ac34eb0
AzureCa <18428368747@sina.cn>
2018-03-14 09:45:32 +0800

    BUG修复

--- src/pages/home/contentManage/infoExpressManage.vue
+++ src/pages/home/contentManage/infoExpressManage.vue
@@ -279,7 +279,7 @@ export default {
     },
     /* 发布 */
     publishSubmit(){
-       this.$confirm('发布后不能修改，确定发布该快报?', '提示', {
+       this.$confirm('确定发布该快报?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
