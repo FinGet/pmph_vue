a076ae5f2515afffffc4dbeadd4fea9815e0294d
gugia <gugia@qq.com>
2018-04-01 03:06:41 +0800

    修改配置文件以适配正式环境

--- config/proxy.js
+++ config/proxy.js
@@ -12,7 +12,7 @@ let proxyTable =  {
     ws: true,
   },
   '/v': {
-    target: 'http://127.0.0.1:11100',//设置你调用的接口域名和端口号 别忘了加http
+    target: 'http://192.168.100.136:11100',//设置你调用的接口域名和端口号 别忘了加http
     changeOrigin: true,               // needed for virtual hosted sites
     ws: true,
   }
