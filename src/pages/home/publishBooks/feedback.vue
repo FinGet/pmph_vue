8879a60b2950c9bbd3e416a9810c74c510a27b67
xiaochengyan <985814585@qq.com>
2018-04-19 13:56:49 +0800

    新增读者反馈

---
+++ src/pages/home/publishBooks/feedback.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="check_error" style="height:100%;">
+    <el-row>
+    <!--  <div class="searchBox-wrapper">
+        <div class="searchName">书名:<span></span></div>
+        <div class="searchInput">
+          <el-input v-model.trim="title" placeholder="请输入" @keyup.enter.native="search"></el-input>
+        </div>
+      </div>-->
+      <div class="searchBox-wrapper searchBox-radio">
+        <div class="searchName">状态:<span></span></div>
+        <el-radio-group v-model="result" class="radio-group" @change="change">
+          <el-radio :label="null">全部</el-radio>
+          <el-radio :label="true">审核</el-radio>
+          <el-radio :label="false">待审核</el-radio>
+        </el-radio-group>
+      </div>
+     <!-- <div class="searchBox-wrapper searchBtn">
+        <el-button  type="primary"  icon="search" @click="search">搜索</el-button>
+      </div>-->
+    </el-row>
+    <el-row>
+      <el-col>
+        <el-table :data="tableData" stripe border style="width: 100%;text-align: center">
+          <el-table-column prop="bookname" label="书名" style="text-align: left;">
+          </el-table-column>
+          <el-table-column prop="feedBackName" label="反馈人">
+          </el-table-column>
+          <!--<el-table-column prop="content" label="反馈内容">
+          </el-table-column>-->
+          <el-table-column prop="result" label="状态">
+            <template scope="scope">
+              <p >{{scope.row.result == true?'已审核':'未审核'}}</p>
+            </template>
+          </el-table-column>
+          <el-table-column prop="authorname" label="审核人">
+         <!-- </el-table-column>
+          <el-table-column prop="authorReply" label="回复内容">-->
+          </el-table-column>
+          <el-table-column prop="authDate" label="审核日期" width="180">
+          </el-table-column>
+          <el-table-column prop="gmtCreate" label="提交时间" width="180">
+          </el-table-column>
+         <!-- <el-table-column prop="result" label="核查结果" width="95" align="center">
+            <template scope="scope">
+              <p v-if="scope.row.isEditorReplied">{{scope.row.result == true?'存在问题':scope.row.result == false?'无问题':'-'}}</p>
+            </template>
+          </el-table-column>-->
+          <el-table-column label="操作" width="80" align="center">
+            <template scope="scope">
+              <router-link type="text" class="link" size="small" :to="{name:'读书反馈详情',query:{id: scope.row.id,type:scope.row.authorReply!=''?'detail':'check'}}">{{scope.row.authorReply!=''?'查看':'审核'}}</router-link>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+    <el-pagination
+      v-if="total>pageSize"
+      class="pull-right marginT10"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageNumber"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+      //  title: "", // 书名
+        result: null, // 检查结果
+        currentPage: 1, // 当前页
+        pageSize: 20,
+        pageNumber: 1,
+        total: 0,
+        tableData: [],
+        startTime: "", // 开始时间
+        endTime: "" // 结束时间
+      };
+    },
+    created() {
+     /* /!* 是否从首页跳转过来 *!/
+      if(this.$route.params.searchInput){
+        this.title=this.$route.params.searchInput;
+      }*/
+      this.getBooks();
+    },
+    methods: {
+      getBooks() {
+        this.$axios
+          .get("/pmpheep/bookCorrection/bookList", {
+            params: {
+              pageSize: this.pageSize,
+              pageNumber: this.pageNumber,
+              //bookname: this.title,
+              result: this.result
+            }
+          })
+          .then(response => {
+            let res = response.data;
+            if (res.code == 1) {
+              this.tableData = res.data.rows;
+              this.total = res.data.total;
+              this.tableData.forEach(item => {
+                item.gmtCreate = this.$commonFun.formatDate(item.gmtCreate);
+                item.authDate = this.$commonFun.formatDate(item.authDate);
+                item.authorReply = this.$commonFun.Empty(item.authorReply)?'':item.authorReply;
+              });
+            }
+          });
+      },
+      /** 搜索*/
+      // search() {
+      //   this.pageSize = 20;
+      //   this.pageNumber = 1;
+      //   this.getBooks();
+      // },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+        this.pageSize = val;
+        this.getBooks();
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+        this.pageNumber = val;
+        this.getBooks();
+      },
+      /** 切换检查结果 */
+      change(val) {
+        this.result = val;
+        this.getBooks();
+      }
+    }
+  };
+</script>
+<style scoped>
+  .search-title {
+    margin: 10px 0 0 10px;
+    font-size: 16px;
+    float: left;
+    height: 36px;
+    line-height: 36px;
+  }
+  .searchBox-radio {
+    width: 380px;
+  }
+  .radio-group {
+    margin-top: 9px;
+    margin-left: 5px;
+  }
+</style>
---
+++ src/pages/home/publishBooks/feedbackDetail.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="check" style="height:100%;">
+    <div class="top-nav">
+      <div class="header_title_tips">
+        <p >{{ruleForm.bookname}}</p>
+        <div class="tips_icon"></div>
+      </div>
+    </div>
+    <div class="bottom-content">
+      <el-row>
+        <el-col :span="12">
+          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="书名:">
+              <p>{{ruleForm.bookname}}</p>
+            </el-form-item>
+
+            <el-form-item label="反馈内容:">
+              <div class="messageBox">
+                <p>{{ruleForm.content}}</p>
+              </div>
+            </el-form-item>
+
+            <el-form-item label="反馈人:">
+              <p>{{ruleForm.feedBackName}}</p>
+            </el-form-item>
+            <el-form-item label="提交时间:">
+              <p>{{ruleForm.gmtCreate}}</p>
+            </el-form-item>
+            <el-form-item label="回复:">
+              <el-input
+                type="textarea"
+                :disabled="type=='detail'"
+                :autosize="{ minRows: 4, maxRows: 8}"
+                placeholder="请输入内容"
+                v-model="ruleForm.authorReply">
+              </el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button icon="arrow-left" @click="back">返回</el-button>
+              <el-button type="primary" :disabled="type=='detail'"  @click="submit('ruleForm')">提交</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        id: "", // 主键id
+        ruleForm: {
+          bookname: "", // 书名
+          page: "",
+          line: "",
+          realname: "", // 纠错人
+          gmtCreate: "", // 提交时间
+          authorReply: ""
+        },
+        type:'',
+        rules: {
+          authorReply: [
+            {
+              type: "string",
+              required: true,
+              message: "请输入回复内容",
+              trigger: "blur"
+            },
+            { min: 1, max: 500, message: "不能超过500个字符", trigger: "change,blur" }
+          ]
+        }
+      };
+    },
+    created() {
+      this.id = this.$route.query.id;
+      this.type = this.$route.query.type;
+      // alert(this.$route.query);
+      this.getBookError();
+    },
+    methods: {
+      /**请求数据 */
+      getBookError() {
+        this.$axios
+          .get("/pmpheep/bookCorrection/bookFeekBackDetail?id="+this.id)
+          .then(response => {
+            console.log(response);
+            let res = response.data;
+            if (res.code == 1) {
+              this.ruleForm = res.data;
+//            this.id = this.ruleForm.id;
+              this.ruleForm.gmtCreate = this.$commonFun.formatDate(
+                this.ruleForm.gmtCreate
+              );
+
+            }
+          });
+      },
+
+      /**提交 */
+      submit(formName) {
+        this.$refs[formName].validate(valid => {
+          if (valid) {
+//          if (this.ruleForm.isAuthorReplied) {
+            this.$axios
+              .put(
+                "/pmpheep/bookCorrection/replyBookFeedBackWriter",
+                this.$initPostData({
+                  id: this.id,
+                  authorReply: this.ruleForm.authorReply
+                })
+              )
+              .then(response => {
+                let res = response.data;
+                if (res.code == 1) {
+                  this.$message.success("提交成功！");
+                  this.back();
+                } else {
+                  this.$message.error(res.msg.msgTrim());
+                }
+              })
+              .catch(err => {
+                this.$message.error("提交失败，请稍后再试！");
+              });
+//          } else {
+//            this.$message.error("主编没有回复，不能提交！");
+//          }
+          } else {
+            console.log("错误提交，请检查输入内容!!");
+            return false;
+          }
+        });
+      },
+      /** 返回上一步*/
+      back() {
+        this.$router.go(-1);
+      }
+    }
+  };
+</script>
+<style>
+  .top-nav {
+    /* float: left; */
+    width: 100%;
+    position: relative;
+    height: 43px;
+    background-color: rgb(238, 241, 246);
+    border: 1px solid rgb(209, 217, 229);
+    box-sizing: border-box;
+  }
+  .top-nav .header_title_tips {
+    margin-bottom: 15px;
+    /* float:left; */
+    color: #fff;
+    position: absolute;
+    left: -8px;
+    top: 8px;
+  }
+  .top-nav .header_title_tips p {
+    /* float:left; */
+    background-color: #12806b;
+    font-size: 16px;
+    padding: 2px 20px 2px 15px;
+    max-width: 545px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .top-nav .header_title_tips .tips_icon {
+    border: 4px solid #475669;
+    position: absolute;
+    left: 0;
+    bottom: -8px;
+    border-left-color: #fff;
+    border-bottom-color: #fff;
+  }
+  .bottom-content {
+    border: 1px solid rgb(209, 217, 229);
+    box-sizing: border-box;
+    border-top: 0;
+    padding: 15px 20px;
+    float: left;
+    width: 100%;
+    background-color: #fff;
+  }
+  .messageBox {
+    min-height: 50px;
+    border: 1px solid #e2dddd;
+    padding: 0px 15px 20px 15px;
+    margin-top: 5px;
+    background: #f3f3f3;
+  }
+</style>
