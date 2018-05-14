5d1528637c8a1076572845d5f954f398d5185eca
xiaochengyan <985814585@qq.com>
2018-05-14 10:19:31 +0800

    配置文件

--- src/common/config.js
+++ src/common/config.js
@@ -7,7 +7,7 @@
 let host = window.location.host;
 
 //默认api请求头地址
-export let BASE_WS_URL = 'ws://39.107.80.79/pmpheep/';
+export let BASE_WS_URL = 'ws://127.0.0.1/pmpheep/';
 export let BASE_URL = 'http://'+host+'/pmpheep/';
 //
 // export const BASE_WS_URL = 'ws://192.168.200.126:8090/pmpheep/';
@@ -17,6 +17,6 @@ export let BASE_URL = 'http://'+host+'/pmpheep/';
 export let DEFAULT_USER_IMAGE = 'http://'+host+'/static/default_image.png';
 export let DEFAULT_BASE_URL = 'http://'+host;
 //请求教材图片根路径地址
-export let teachPicUrl ='http://39.107.80.79';
+export let teachPicUrl ='http://120.76.221.250';
 
-export let IS_DEBUG = !host.includes('39.107.80.79');
+export let IS_DEBUG = !host.includes('120.76.221.250');
