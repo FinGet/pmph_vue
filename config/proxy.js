f7c117c9ef4ffd893f7e6ed8d046cd54ed045cad
xiaochengyan <985814585@qq.com>
2018-06-28 14:50:02 +0800

    视频播放

--- config/proxy.js
+++ config/proxy.js
@@ -6,15 +6,15 @@ let proxyTable =  {
     changeOrigin: true,               // needed for virtual hosted sites
     ws: true,
   },
-  'pmph_vedio':{
+  '/v':{
     target: 'http://127.0.0.1:11000',//设置你调用的接口域名和端口号 别忘了加http
     changeOrigin: true,               // needed for virtual hosted sites
     ws: true,
   },
-  '/v': {
+  /*'/v': {
     target: 'http://192.168.100.136:11100',//设置你调用的接口域名和端口号 别忘了加http
     changeOrigin: true,               // needed for virtual hosted sites
     ws: true,
-  }
+  }*/
 };
 module.exports = proxyTable;
